c1ce1ece27195121b7d5ae6d0d2ee03d34dd40e6
Bram Alders <531131@student.fontys.nl>
2025-03-27 17:30:45 +0100

    added a top layer to the textbox so it isn't a hard cut.

--- src/components/text_field.tsx
+++ src/components/text_field.tsx
@@ -4,6 +4,11 @@ const TextField: React.FC = () => {
     return (
         <div className="text_field_container">
             <div className="text_field_left"></div>
+            <div className="text_field_top_container">
+                <div className="text_field_left_corner"></div>
+                <div className="text_field_top"></div>
+                <div className="text_field_right_corner"></div>
+            </div>
             <div className="text_field"></div>
             <div className="text_field_right"></div>
         </div>
