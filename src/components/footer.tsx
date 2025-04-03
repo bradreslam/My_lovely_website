9ea08bfc54e733360b512b803bd7847ef997691b
bradreslam <105541244+bradreslam@users.noreply.github.com>
2025-04-03 08:21:19 +0200

    added mobile responsive, and added some temp text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <div>
-            <img className="left_tower" src="src/assets/left_tower.png" alt="left_tower"/>
-            <img className="right_tower" src="src/assets/right_tower.png" alt="right_tower"/>
+            <img className="left_tower" alt="left_tower"/>
+            <img className="right_tower" alt="right_tower"/>
             <footer className="footer">
             </footer>
         </div>
