bebc6361bcdb944aa143adfa4a4d40e4c43ab85f
bradreslam <105541244+bradreslam@users.noreply.github.com>
2025-07-24 21:08:38 +0200

    Updated my maze code and blocked of the maze entrance.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,37 @@
 import {useNavigate} from "react-router-dom";
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Footer: React.FC = () => {
     const navigate = useNavigate();
+    const [doorPosition, setDoorPosition] = useState(0)
+    const [clickCount, setClickCount] = useState(0);
+    const [doorOpen, setDoorOpen] = useState(false);
+
+    useEffect(() => {
+        setDoorPosition(Math.random())
+    }, []);
 
     const handleClick = () => {
-        navigate('/maze/1/2');
+        if(doorOpen){
+            navigate('/maze/1/2');
+        }
+        if(clickCount < 4){
+            setClickCount(clickCount+1)
+            console.log(clickCount)
+        }
+        else{
+            setDoorOpen(true);
+        }
     };
 
     return (
         <div className="footer">
             <div className="left_tower"/>
             <div className="right_tower"/>
-            <button className="maze_door" onClick={handleClick} style={{right: `${Math.random() * (95 - 5) + 5}%`}}/>
+            <button className="maze_door" onClick={handleClick} style={{right: `${doorPosition * (90 - 10) + 10}%`}}>
+                <image className="maze_door_block" style={{ transform: doorOpen? `translateY(65px)`: 'none',
+                    transition: 'transform 1s ease'}}/>
+            </button>
         </div>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://i531131.hera.fontysict.net/"
+  base: "https://i531131.hera.fontysict.net/",
+  build: {
+    sourcemap: true
+  }
 })
